91fffb6cbbbfbb1723831d31b4dece0c1c6579f9
Ayhan Bizden <ayhan.bizden@gmail.com>
2025-07-11 11:17:06 +0300

    feat: integrate Google Analytics with gtag.js for improved tracking and analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
@@ -10,15 +11,31 @@ export default function App({ Component, pageProps, err }: AppProps & { err: Err
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <SessionProvider session={pageProps.session}>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <CartProvider>
-            <Component {...pageProps} err={err} />
-          </CartProvider>
-        </Hydrate>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </SessionProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-C9MMBP55Y7`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C9MMBP55Y7', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <SessionProvider session={pageProps.session}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <CartProvider>
+              <Component {...pageProps} err={err} />
+            </CartProvider>
+          </Hydrate>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
   );
 }
