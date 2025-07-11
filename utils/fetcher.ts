c5fdbcf3c46a9e3f5639e34fc2d6730eb5e36b00
Ayhan Bizden <ayhan.bizden@gmail.com>
2025-07-11 08:37:34 +0300

    chore: update dependencies and add Sentry profiling support; introduce ESLint cache file

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,7 @@
-import type { AnySchema, InferType } from "yup";
-import { ResponseError } from "./responseError";
-import type { HTTPMethod } from "./types";
+import type { AnySchema, InferType } from 'yup';
+import { ResponseError } from './responseError';
+import type { HTTPMethod } from './types';
+import * as Sentry from '@sentry/nextjs';
 
 type FetcherConfig<Schema extends AnySchema | null> = {
   readonly method: HTTPMethod;
@@ -11,25 +12,25 @@ type FetcherConfig<Schema extends AnySchema | null> = {
 
 export async function fetcher<Schema extends null>(
   path: string,
-  { method, body, config, schema }: FetcherConfig<Schema>
+  { method, body, config, schema }: FetcherConfig<Schema>,
 ): Promise<null>;
 
 export async function fetcher<Schema extends AnySchema>(
   path: string,
-  { method, body, config, schema }: FetcherConfig<Schema>
+  { method, body, config, schema }: FetcherConfig<Schema>,
 ): Promise<InferType<Schema>>;
 
 export async function fetcher<Schema extends AnySchema | null>(
   path: string,
-  { method, body, config, schema }: FetcherConfig<Schema>
+  { method, body, config, schema }: FetcherConfig<Schema>,
 ) {
   try {
     const response = await fetch(path, {
       ...config,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      credentials: "include",
+      credentials: 'include',
       method,
       ...(body && { body: JSON.stringify(body) }),
     });
@@ -42,11 +43,13 @@ export async function fetcher<Schema extends AnySchema | null>(
 
       return schema.cast(data);
     }
+    Sentry.captureException(response.statusText);
     throw new ResponseError(response.statusText, response.status);
   } catch (err) {
     if (err instanceof ResponseError) {
       throw err;
     }
-    throw new ResponseError("Something went wrong during fetching!");
+    Sentry.captureException(err);
+    throw new ResponseError('Something went wrong during fetching!');
   }
 }
